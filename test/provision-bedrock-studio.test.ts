02baa7ad437ededd378298080e96c66777387123
Jorke Odolphi <jorkeo@amazon.com>
2024-05-09 11:59:38 +1000

    Initial commit

---
+++ test/provision-bedrock-studio.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ProvisionBedrockStudio from '../lib/provision-bedrock-studio-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/provision-bedrock-studio-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ProvisionBedrockStudio.ProvisionBedrockStudioStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
